a13bd7d629f461f68959b646f255ebb9232447ff
Jason Pope <cowholio4@gmail.com>
2018-10-29 21:17:04 -0700

    Remove IP generation from timer

--- performance_tests/test_bloom_performance.py
+++ performance_tests/test_bloom_performance.py
@@ -3,23 +3,28 @@ from ipaddress import IPv4Address
 from seclytics.bloom_category import BloomCategory
 
 
-run_count = 100000
+run_count = 1000000
+ips = list()
+
+for i in range(0, run_count):
+    # 153,099 random ips generated per second
+    bits = getrandbits(32)
+    addr = str(IPv4Address(bits))
+    ips.append(addr)
+
+bloom = BloomCategory(malicious_path='/tmp/malicious-ips.bloom',
+                      predicted_path='/tmp/predicted-ips.bloom',
+                      has_intel_path='/tmp/ip-threat-intel.bloom')
 
 
 def test():
-    bloom = BloomCategory(malicious_path='/tmp/malicious-ips.bloom',
-                          predicted_path='/tmp/predicted-ips.bloom',
-                          has_intel_path='/tmp/ip-threat-intel.bloom')
-
-    for i in range(0, run_count):
-        # 153,099 random ips generated per second
-        bits = getrandbits(32)
-        addr = str(IPv4Address(bits))
-        bloom.check_ip(addr)
+    for ip in ips:
+        bloom.check_ip(ip)
     # benchmark(bulk_features_pandas_ip_metadata, ips)
 
 
 if __name__ == '__main__':
     import timeit
+    test()
     time = timeit.timeit("test()", setup="from __main__ import test", number=1)
     print(run_count/time)
