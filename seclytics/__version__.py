ef095cf80761372a844ad302f7f567942810d3fe
Jason Pope <cowholio4@gmail.com>
2019-10-25 15:30:46 -0700

    Increment version

--- seclytics/__version__.py
+++ seclytics/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'seclytics'
 __description__ = 'Seclytics Threat Intelligence API.'
 __url__ = 'https://www.seclytics.com/'
-__version__ = '0.1.5'
+__version__ = '0.2.0'
 __author__ = 'Jason pope'
 __author_email__ = 'jpope@seclytics.com'
 __license__ = 'Apache License 2.0'
