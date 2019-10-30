f129234a8ba372762626c69e7fe11f7f8db6c59a
Jason Pope <cowholio4@gmail.com>
2019-10-30 16:53:57 -0700

    Force python 2 as str

--- seclytics/bloom_category.py
+++ seclytics/bloom_category.py
@@ -3,6 +3,7 @@ from .portable_bloom import PortableBloom
 from enum import Enum
 
 
+
 class Category(Enum):
     malicious = 1
     predicted = 2
@@ -18,8 +19,14 @@ class BloomCategory(object):
     def check_ip(self, ip, check_suspicious=True, check_predicted=True,
                  check_malicious=True):
         value = ip
-        if type(value) == str:
-            value = unicode(ip)
+<<<<<<< Updated upstream
+        if type(value) != str and six.PY2:
+            raise RuntimeError("Only accepts str")
+=======
+        if type(value) == str and six.PY2:
+            value = unicode(ip).encode('ascii')
+            
+>>>>>>> Stashed changes
 
         # To reduce bloom filter checks we store ALL ips in has_intel
         # This way the majority of IPs will only have to check once.
