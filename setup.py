0fca10e28cb1ea7a059b86288794209c29e714cc
Jason Pope <cowholio4@gmail.com>
2019-11-18 14:24:03 -0800

    Prep for pypi release
    Add some missing fields to setup.py
    Add required file 'setup.cfg'

--- setup.py
+++ setup.py
@@ -45,12 +45,23 @@ setup(
     version=about['__version__'],
     description=about['__description__'],
     long_description='Get threat intelligence on IPs, CIDRs, ASNs and Files using Seclytics.',
+    keywords=['threat intel', 'cybersecurity'],
 
     # The project's main homepage.
     url=about['__url__'],
     entry_points = {
         'console_scripts': ['seclytics_db_download=seclytics.scripts.download_db:main'],
     },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7'
+    ],
     # Author details
     cmdclass={'test': PyTest},
     author=about['__author__'],
